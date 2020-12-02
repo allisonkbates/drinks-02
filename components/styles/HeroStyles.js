2dfd3fed6436649e1a1cb293266a9f0b08740826
Allison <allisonkbates@gmail.com>
2020-12-01 22:53:00 -0500

    added box shadow

--- components/styles/HeroStyles.js
+++ components/styles/HeroStyles.js
@@ -41,6 +41,7 @@ const StyledHeroCard = styled.div `
 	background-color: #FFFFFF;
 	margin: 110px 0px 0px;
 	color: #222222;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
 	@media only screen and (min-width: 768px) {
 		height: 220px;
 		width: 300px;
