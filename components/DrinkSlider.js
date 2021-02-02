9c26ccc249f67c440a62f9ec4ff7a7b2fe1a7939
Allison <allisonkbates@gmail.com>
2021-02-01 22:26:53 -0500

    updates to responsiveness

--- components/DrinkSlider.js
+++ components/DrinkSlider.js
@@ -2,6 +2,17 @@ import Link from 'next/link';
 import Slider from "react-slick";
 import { getFilteredDrinks } from '../helpers/drinkFormat';
 
+function HideArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "none" }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default function DrinkSlider(props) {	
 	const drinks = getFilteredDrinks(props.drinks, props.allTags, props.count); /* Re-evaluate if this is the BEST way to do this */
 
@@ -9,15 +20,17 @@ export default function DrinkSlider(props) {
 	let settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+		speed: 500,
+		nextArrow: <HideArrow />,
+		prevArrow: <HideArrow />,
     slidesToShow: 4,
-		slidesToScroll: 3,
+		slidesToScroll: 1,
 		responsive: [
 		{
 			breakpoint: 600,
 			settings: {
-				slidesToShow: 1.5,
-				slidesToScroll: 2,
+				slidesToShow: 1.75,
+				slidesToScroll: 1,
 				infinite: true,
 				dots: true
 			}
@@ -29,7 +42,7 @@ export default function DrinkSlider(props) {
 		<div className="rec__container">
 			<div className="inline">
 				<h2 className={`rec__heading ${props.size}`}>{props.label}</h2>
-		{/*<Link href="/drinks">
+			{/*<Link href="/drinks">
 					<a className="drink__body--link inline-link">View All</a>
 				</Link> */}
 			</div>
--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<div 
-			style={{backgroundColor: "#377084", height: "40px", marginTop: "10px"}}>
+			style={{backgroundColor: "#377084", height: "40px", marginTop: "32px"}}>
 		</div>
 	)
 }
