b8b3bd2463b6c113eecf3639a9a87367fffcd653
Allison Mazzetti <allisonkbates@gmail.com>
2022-01-25 09:45:07 -0500

    refactored ingredients list

--- components/IngredientList.js
+++ components/IngredientList.js
@@ -2,14 +2,24 @@ export default function IngredientList(props) {
   /* Assign Props */
   let handleChange = props.handleChange;
   let ingredients = props.ingredients;
-  
-  /* Organize Ingredients into Categories */
+
   const FILTER_ING_MAP = {
-    Alcohol: ing => ing.fields.ingredientType === 'alcohol',
-    Liqueurs: ing => ing.fields.ingredientType === 'liqueurs',
-    Bitters: ing => ing.fields.ingredientType === 'bitters',
-    Other: ing => ing.fields.ingredientType === "wines-vermouths" || ing.fields.ingredientType === "sugars",
-    Grocery: ing => ing.fields.ingredientType === 'grocery',
+    Alcohol: {
+      filter: ing => ing.fields.ingredientType === 'alcohol',
+      title: 'Alcohol'
+    },
+    Liqueurs: {
+      filter: ing => ing.fields.ingredientType === 'liqueurs' || ing.fields.ingredientType === 'bitters',
+      title: 'Liqueurs & Bitters'
+    },
+    Other: {
+      filter: ing => ing.fields.ingredientType === "wines-vermouths",
+      title: 'Wines & Vermouths'
+    },
+    Grocery: {
+      filter: ing => ing.fields.ingredientType === 'grocery' || ing.fields.ingredientType === "sugars",
+      title: 'Grocery Items',
+    }
   }
   
   const FILTER_ING_NAMES = Object.keys(FILTER_ING_MAP);
@@ -17,7 +27,7 @@ export default function IngredientList(props) {
   let categoriesWithIng = FILTER_ING_NAMES
   .map(category => {
     let ingredientList = ingredients
-    .filter(FILTER_ING_MAP[category])
+    .filter(FILTER_ING_MAP[category]['filter'])
     .map(ing => (
       <div key={ing.id}>
         <input type="checkbox" name="inStock" defaultChecked={ing.fields.inStock} id={ing.id} onChange={handleChange}></input>
@@ -27,7 +37,7 @@ export default function IngredientList(props) {
     )
     return (
       <>
-      <h2>{category}</h2>
+      <h2>{FILTER_ING_MAP[category]['title']}</h2>
       <div>
         {ingredientList}
       </div>
